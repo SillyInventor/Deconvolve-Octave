b3b780f14a3dcadd3ec5fd8030cd070a40699549
Silly Inventor <jamesdavis3004@gmail.com>
2019-05-11 20:53:49 -0400

    Still working on adding TATR

--- Deconvolve.m
+++ Deconvolve.m
@@ -5,65 +5,91 @@ Water = csvread ("C:/Users/JamesDavis/Desktop/Work/CISUVC/Water Scatter Spec - 5
 N500 = csvread ("C:/Users/JamesDavis/Desktop/Work/CISUVC/500ppm Scatter Spec - 5-5-19.csv");
 N1000 = csvread ("C:/Users/JamesDavis/Desktop/Work/CISUVC/1000ppm Scatter Spec - 5-5-19.csv");
 
+increment=.01;
+cw=2; % cw = Convolution width of the physical convolution process (unknown)
+dw=2.01; % dw = Deconvolution width (estimated) must equal cw for perfect results
+SmoothWidth=6; % Width of final smoothing to remove high-frequency noise
+nmToCm = 1e-7;
+LEDCenterWavelength = 253.75;
+
+%Turn columns into rows
 Wavelengths = Wavelengths';
-WavelengthsHalf =198.87:.054904:426.00;
 LED = LED';
 Water = Water';
 N500 = N500';
 N1000 = N1000';
 
-LED = nanfastsmooth(LED,30);
-Water = nanfastsmooth(Water,30);
-N500 = nanfastsmooth(N500,30);
-N1000 = nanfastsmooth(N1000,30);
+%Shift in wavenumber
+NitrateShift = 725.1664843;
+Interference1 = 404;
+Interference2 = 1124;
+Interference3 = 1185;
+Interference4 = 1518;
 
-SumLED = sum(LED);
-maxVal = 10e30;
+%Shift in wavelength
+NitrateWavelength = 1/(nmToCm*(1/(nmToCm*LEDCenterWavelength)-NitrateShift));
+Interference1Wavelength = 1/(nmToCm*(1/(nmToCm*LEDCenterWavelength)-Interference1));
+Interference2Wavelength = 1/(nmToCm*(1/(nmToCm*LEDCenterWavelength)-Interference2));
+Interference3Wavelength = 1/(nmToCm*(1/(nmToCm*LEDCenterWavelength)-Interference3));
+Interference4Wavelength = 1/(nmToCm*(1/(nmToCm*LEDCenterWavelength)-Interference4));
 
-WaterDeconv = deconv([Water 1:4136],LED).*sum(LED);
-indices = find(WaterDeconv>maxVal);
-WaterDeconv(indices) = maxVal;
-indices = find(WaterDeconv<-maxVal);
-WaterDeconv(indices) = -maxVal;
+%Shift in index
+[d, LEDCenterIndex] = min(abs(Wavelengths-LEDCenterWavelength));
+[d, NitrateIndex] = min(abs(Wavelengths-NitrateWavelength));
+[d, Interference1Index] = min(abs(Wavelengths-Interference1Wavelength));
+[d, Interference2Index] = min(abs(Wavelengths-Interference2Wavelength));
+[d, Interference3Index] = min(abs(Wavelengths-Interference3Wavelength));
+[d, Interference4Index] = min(abs(Wavelengths-Interference4Wavelength));
 
-N500Deconv = deconv([N500 1:4136],LED).*sum(LED);
-indices = find(N500Deconv>maxVal);
-N500Deconv(indices) = maxVal;
-indices = find(N500Deconv<-maxVal);
-N500Deconv(indices) = -maxVal;
+%Smoothing Spectra
+SmoothLength = 30;
+LED(SmoothLength:arrayLength-SmoothLength) = nanfastsmooth(LED(SmoothLength:arrayLength-SmoothLength),SmoothLength);
+LED = LED./max(LED);
 
-N1000Deconv = deconv([N1000 1:4136],LED).*sum(LED);
-indices = find(N1000Deconv>maxVal);
-N1000Deconv(indices) = maxVal;
-indices = find(N1000Deconv<-maxVal);
-N1000Deconv(indices) = -maxVal;
+Water = nanfastsmooth(Water,19);
+Water(1:201) = nanfastsmooth(Water(1:201),50);
+Water(700:2001) = nanfastsmooth(Water(700:2001),50);
+Water = Water./max(Water);
+Water = Water.*1.3;
 
+N500 = nanfastsmooth(N500,19);
+N500(1:201) = nanfastsmooth(N500(1:201),50);
+N500(700:2001) = nanfastsmooth(N500(700:2001),50);
+N500 = N500./max(N500);
+N500 = N500.*1.3;
 
-ydc=deconv(yc,c).*sum(c);     % Attempt to recover y by deconvoluting c from yc
+N1000 = nanfastsmooth(N1000,19);
+N1000(1:201) = nanfastsmooth(N1000(1:201),50);
+N1000(700:2001) = nanfastsmooth(N1000(700:2001),50);
+N1000 = N1000./max(N1000);
+N1000 = N1000.*1.3;
 
-%}
+%Proposed Spectral components
+PossibleSpectrum = Water;
+RegularizationConstant = 100;
+WeightConstant = 100;
 
-x=0:.01:20;y=zeros(size(x));
-temp=0:.005:20;
-y(900:1100)=1;                % Create a rectangular function y, 
-                              % 200 points wide
-y=y+.01.*randn(size(y));      % Noise added before the convolution
-c1=exp((0:200)./30);    % exponential trailing convolution 
-c2=exp(-(1:1800)./30);    % exponential trailing convolution
-c = [c1 c2];
-c = c./(max(c)+1);
-yc=conv([y 1:2000],c,'full')./sum(c);  % Create exponential trailing rectangular
-                              % function, yc
-% yc=yc+.01.*randn(size(yc)); % Noise added after the convolution
-ydc=deconv(yc,c).*sum(c);     % Attempt to recover y by deconvoluting c from yc
-% The sum(c2) is included simply to scale the amplitude of the result to match the original y.
+PossibleSpectrumDerivative = diff(PossibleSpectrum)./2;
+PossibleSpectrumSecoundDerivative = diff(PossibleSpectrumDerivative)./2;
+ConvolvedPossibleSpectrum = abs(ifft(fft(PossibleSpectrum).*fft(LED))./sum(LED));
+ConvolvedPossibleSpectrumDerivative = abs(ifft(fft(PossibleSpectrumDerivative).*fft(LED))./sum(LED));
+WeightFunction = e^(-(ConvolvedPossibleSpectrumDerivative/WeightConstant)^2);
+
+Q=0.5*sum((ConvolvedPossibleSpectrum.-Water).^2)+regularizationConstant*sum(WeightFunction.*(PossibleSpectrumDerivative.^2));
+regularizationConstant /= 2;
+
+% Now attempt to recover the original signal by deconvolution (2 methods)
+%WaterDeconv=ifft(fft(Water)./fft(LED)).*sum(LED);  % Deconvolution by fft/ifft
+%WaterDeconv=abs(fastsmooth(WaterDeconv,SmoothWidth,5));
+
+%N500Deconv=ifft(fft(N500)./fft(LED)).*sum(LED);  % Deconvolution by fft/ifft
+%N500Deconv=abs(fastsmooth(N500Deconv,SmoothWidth,5));
+
+%N1000Deconv=ifft(fft(N1000)./fft(LED)).*sum(LED);  % Deconvolution by fft/ifft
+%N1000Deconv=abs(fastsmooth(N1000Deconv,SmoothWidth,5));
+
 % Plot all the steps
-indices = find(ydc>maxVal);
-ydc(indices) = maxVal;
-indices = find(ydc<-maxVal);
-ydc(indices) = -maxVal;
-
-subplot(2,2,1); plot(Wavelengths,LED); title('LED');
-subplot(2,2,2); plot(WavelengthsHalf,WaterDeconv);title('water');
-subplot(2,2,3); plot(WavelengthsHalf,N500Deconv); title('500ppm nitrate');
-subplot(2,2,4); plot(WavelengthsHalf,N1000Deconv);title('1000ppm nitrate')
+%subplot(2,2,1); plot(Wavelengths,LED); title('LED');
+%subplot(2,2,2); plot(Wavelengths,WaterDeconv);title('Water'); 
+%subplot(2,2,3); plot(Wavelengths,N500Deconv); title('500 ppm nitrate');
+%subplot(2,2,4); plot(Wavelengths,N1000Deconv);title('1000 ppm nitrate');
